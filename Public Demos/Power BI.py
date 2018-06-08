720128465c0706fd1bba815d1a84e4d38ed7d924
franklin8705 <franklin8705@gmail.com>
2018-06-08 01:21:43 -0400

    Update: Public Demos README.md notebooks

---
+++ Public Demos/Power BI.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Connect The Churn Data to Power BI
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Steps
+# MAGIC - Premium Service
+# MAGIC - Power BI application
+# MAGIC - Authentication: Access Token
+# MAGIC - JDBC URL
+
+# COMMAND ----------
+
